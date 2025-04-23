f34bb27ca777b630afac553f2e07f77b36848fab
Subhadip Das <subhadipdas911@gmail.com>
2025-04-23 14:02:31 +0530

    Useing Examples to perform DDT using Integer, Double and String

--- src/test/java/Runners/BillingTestRunner.java
+++ src/test/java/Runners/BillingTestRunner.java
@@ -1,15 +1,15 @@
 package Runners;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {".//src/test/resources/Features/Login.feature"},
+        features = {"src/test/resources/Features/Billing.feature"},
         glue = {"StepDefinitions"},
-        plugin = {"pretty","html:test-output"}
+        dryRun = false,
+        plugin = {"pretty"}
 )
-public class TestRunner {
+public class BillingTestRunner {
 }
