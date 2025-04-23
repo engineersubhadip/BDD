f1be8ff1b42e7e723a0be3f0a092203fb70889b8
Subhadip Das <subhadipdas911@gmail.com>
2025-04-23 10:36:59 +0530

    @Before and @After hooks with 2 scenarios and multiple @Before and @After

--- src/test/java/Hooks/SearchHook.java
+++ src/test/java/Hooks/SearchHook.java
@@ -6,13 +6,23 @@ import io.cucumber.java.Before;
 
 public class SearchHook {
 
-    @Before
-    public void setUp() {
+    @Before(order=1)
+    public void setUpBrowser() {
         System.out.println("@Before getting executed..... Driver has been initialized");
     }
 
-    @After
-    public void tearDown() {
+    @Before(order=2)
+    public void setUpDataBase() {
+        System.out.println("@Before getting executed..... DataBase has been initialized");
+    }
+
+    @After(order=2)
+    public void tearDownDataBase() {
+        System.out.println("@After getting executed .............. DataBase is now being closed");
+    }
+
+    @After(order=1)
+    public void tearDownBrowser() {
         System.out.println("@After getting executed .............. Driver is now being closed");
     }
 }
